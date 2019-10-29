a6ccf44021c4efda89e199028ef8f14b38e07461
Sawn39 <33158086+Sawn39@users.noreply.github.com>
2019-10-29 13:40:59 +0100

    pagina principal hecha

--- practica06/js/script.js
+++ practica06/js/script.js
@@ -11,30 +11,28 @@ function $$(selector, numero) {
 
 
 // Sentencia que se ejecuta al cargar la página
-document.addEventListener("load", load, false);
+document.addEventListener("DOMContentLoaded", load, false);
 
 
 function load() {
     
-    // Control del formulario de inicio de sesión
-    $("#formini>form").addEventListener("submit", comprobarLogin);
 }
 
 
 function comprobarLogin() {
-    var nombre_ = $$("#formini>form>input", 0),
-        password_ = $$("#formini>form>input", 1),
+    var nombre_ = $$("#formini>form>input", 0).value,
+        password_ = $$("#formini>form>input", 1).value,
         devuelve_ = false;
 
-    if (nombre_.value != ""  &&  password_.value != "") {
-        let caracteres1_ = 0, caracteres2_ = 0;
+    if (nombre_ != ""  &&  password_ != "") {
+        var caracteres1_ = 0, caracteres2_ = 0;
 
-        for (let i=0; i<nombre_.length && i<password_.length; i++) {
+        for (let i=0; i<nombre_.length || i<password_.length; i++) {
             caracteres1_ += (i<nombre_.length && nombre_.charAt(i)!=" ") ? 1 : 0;
             caracteres2_ += (i<password_.length && password_.charAt(i)!=" ") ? 1 : 0;
         }
         
-        devuelve = (caracteres1_==nombre_.length && caracteres2_==password_.length) ? true : false;
+        devuelve_ = (caracteres1_==0 || caracteres2_==0) ? false : true;
     }
 
     return devuelve_;
