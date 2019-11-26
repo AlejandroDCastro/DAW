5e64da95428aebd42382eb4f1097e3b1547074f8
Sawn39 <33158086+Sawn39@users.noreply.github.com>
2019-11-26 01:14:44 +0100

    preparando p09

---
+++ practica09/busqueda.php
@@ -0,0 +1,56 @@
+<?php
+    session_start();
+
+    $titulo = "Búsqueda - Pictures & Images";
+    if (isset($_SESSION["logueado"])) {
+        $estilo = $_SESSION["estilo"];
+    } else {
+        $estilo = "css/style.css";
+    }
+    
+    // Incluímos el head con el doctype
+    require_once("head.php");
+
+    // Incluímos la etiqueta <body> junto al header
+    require_once("header.php");
+?>
+    <!-- CONTENIDO: Título contenido, Formulario -->
+    <main>
+        <section>
+            <h1>Búsqueda de Foto</h1>
+            <div class="printCentro">
+                <form action="resultado.php" id="formulario" method="POST">
+                    <div>
+                        <label for="title">Título:</label>
+                        <input type="text" name="titulo" id="title" placeholder="Introduce el título" class="formulario">
+                    </div>
+
+                    <div>
+                        <label for="date">Fecha:</label>
+                        <input type="date" name="fecha" id="date" class="formulario">
+                    </div>
+
+                    <div>
+                        <label for="country">País:</label>
+                        <input list="countries" name="pais" id="country" placeholder="Introduce tu país" class="formulario">
+                        <datalist id="countries">
+                            <?php
+                                require_once("paises.php");
+                            ?>
+                        </datalist>
+                    </div>
+                    <div>
+                        <label for="author">Autor:</label>
+                        <input type="text" name="autor" id="author" placeholder="Introduce el autor" class="formulario">
+                    </div>
+                    <div>
+                        <input type="submit" value="Buscar">
+                    </div>
+                </form>
+            </div>
+        </section>
+    </main>
+
+<?php
+    require_once("footer.php");
+?>
---
+++ practica09/index.php
@@ -0,0 +1,154 @@
+<?php
+
+    // Iniciar una nueva sesión o reanudar la existente....
+	session_start();
+
+    //La primera vez que entra a la pagina y se esta recordando, va directamente a acceso.
+    if(isset($_COOKIE['recordar']) && !isset($_SESSION["logueado"]))
+    {
+        $host = $_SERVER['HTTP_HOST'];                         
+        $uri  = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');    
+        $extra = 'acceso.php';
+        header("Location: http://$host$uri/$extra");   
+    }
+
+    else
+    {
+
+        $titulo = "Inicio - Pictures & Images";
+        if (isset($_SESSION["logueado"])) {
+            $estilo = $_SESSION["estilo"];
+        } else {
+            $estilo = "css/style.css";
+        }
+
+        // Incluímos el head con el doctype
+        require_once("head.php");
+
+        // Incluímos la etiqueta <body> junto al header
+        require_once("header.php");
+?>
+
+<main>
+    <section>
+        <h1>Inicio</h1>
+        
+        <?php
+
+        //Si existe la cookie de usuario, permitimos que acceda a la parte privada directamente.
+            if(isset($_COOKIE['recordar']))
+            {
+                //Convertimos el string a array.
+                $datos = $_COOKIE['recordar'];
+                $datosArray = json_decode($datos,true);
+                $dia = $datosArray[2]['mday'];
+                $mes = $datosArray[2]['mon'];
+                $anyo = $datosArray[2]['year'];
+                $minutos = $datosArray[2]['minutes'];
+                $horas = $datosArray[2]['hours'];
+
+                echo"<p style = text-align:center;>Hola <b>$datosArray[0]</b>, 
+                su última visita fue el <b>$dia/$mes/$anyo</b> 
+                a las <b>$horas:$minutos</b></p>";
+
+            }
+
+            if(!isset($_SESSION["logueado"])) {
+
+        ?>
+        
+        <!-- Formulario para acceder como usuario registrado -->
+        <section id="formini">
+            <h2>Inicia Sesión</h2>
+
+            <!--Miramos si en la url nos ha llegado un fallo y se lo mostramos al usuario-->
+            <?php 
+
+                if(isset($_GET["fallo"])) {
+                    if ($_GET["fallo"] == 1) {
+                        echo "<h3 style='color:red; text-align:center;'>Usuario o contraseña incorrectos</h3>";
+                    } elseif ($_GET["fallo"] == 2) {
+                        echo "<h3 style='color:red; text-align:center;'>Acceso denegado. Login necesario.</h3>";
+                    }
+                }
+
+            ?>
+
+            <form action="acceso.php" method="POST">
+                <label for="nombre">Nombre: </label>
+                <input type="text" name="nombre" id="nombre" placeholder="Introduce tu nombre">
+
+                <label for="pass">Contraseña: </label>
+                <input type="password" name="pass" id="pass" placeholder="Introduce tu contraseña">
+                
+                <label for="recuerdame">Recuerdame:</label>
+                <input type="checkbox" name="recuerdame" id="recuerdame" value="si">
+                
+                <input type="submit" value="Entrar">
+            </form>
+            <p>¿Aún no te has registrado? <a href="registro.php">Regístrate</a></p>
+        </section>
+
+        <?php
+
+            }
+
+        ?>
+
+        <!-- Carga las últimas 5 fotos -->
+        <section>
+            <h2>Últimas Fotos</h2>
+            <div class="seccionfoto">
+                <article>
+                    <a href="detalle.php?id=1">
+                        <img width="400" src="Images/arbol.jpg" alt="Arbol">
+                    </a>
+                    <h3><a href="detalle.php?id=1">Porque los árboles no dan Wi-Fi</a></h3>
+                    <p>Fecha: 15-01-2018 20:55</p>
+                    <p>Autor: Kevin Serna García</p>
+                    <p>País: España</p>
+                </article>
+                <article>
+                    <a href="detalle.php?id=2">
+                        <img width="400" src="Images/gallina.jpg" alt="Gallina">
+                    </a>
+                    <h3><a href="detalle.php?id=2">Gallinas que no sufren</a></h3>
+                    <p>Fecha: 01-05-2019 15:34</p>
+                    <p>Autor: Miguel Hernández</p>
+                    <p>País: Suiza</p>
+                </article>
+                <article>
+                    <a href="detalle.php?id=3">
+                        <img width="400" src="Images/burro.jpg" alt="Burro">
+                    </a>
+                    <h3><a href="detalle.php?id=3">Cuando veo a mi crush</a></h3>
+                    <p>Fecha: 27-02-2017 23:00</p>
+                    <p>Autor: José Manuel García Baeza</p>
+                    <p>País: Argentina</p>
+                </article>
+                <article>
+                    <a href="detalle.php?id=4">
+                        <img width="400" src="Images/estudiante.jpg" alt="Estudiante">
+                    </a>
+                    <h3><a href="detalle.php?id=4">A estudiar que ya es hora</a></h3>
+                    <p>Fecha: 02-08-2017 10:30</p>
+                    <p>Autor: Pedro Sánchez</p>
+                    <p>País: Andorra</p>
+                </article>
+                <article>
+                    <a href="detalle.php?id=5">
+                        <img width="400" src="Images/synth.jpg" alt="Synth">
+                    </a>
+                    <h3><a href="detalle.php?id=5">Ilusión infinita</a></h3>
+                    <p>Fecha: 20-12-2016 00:55</p>
+                    <p>Autor: Albert Rivera</p>
+                    <p>País: Chile</p>
+                </article>
+            </div>
+        </section>
+    </section>
+</main>
+<?php
+    require_once("footer.php");
+}
+?>
