0f29912150aff5340277c543e27a04d03fe19c8a
Sawn39 <33158086+Sawn39@users.noreply.github.com>
2019-12-15 16:59:54 +0100

    tema de ficheros terminado

--- practica11/index.php
+++ practica11/index.php
@@ -108,7 +108,7 @@
         echo
         "<!-- Carga las últimas 5 fotos -->
         <section>
-            <h2>Últimas Fotos</h2>
+            <h2 style='padding-bottom:30px;'>Últimas Fotos</h2>
             <div class='seccionfoto'>";
             while($fila = $resultado->fetch_object()) {
                 echo "<article>
@@ -116,13 +116,13 @@
                         <img width='400' src='$fila->Fichero' alt='$fila->Alternativo'>
                     </a>
                     <h3><a href='detalle.php?id=$fila->IdFoto'>$fila->Titulo</a></h3>
-                    <p>Fecha: $fila->FRegistro</p>";
+                    <p><b>Fecha</b>: $fila->FRegistro</p>";
                     //Si no tiene pais, no mostramos ese campo.
                     if(!($fila->NomPais == ""))
                     {
-                        echo "<p>País: $fila->NomPais</p>";
+                        echo "<p><b>País</b>: $fila->NomPais</p>";
                     } else {
-                        echo "<p>País: No consta</p>";
+                        echo "<p><b>País</b>: No consta</p>";
                     }
                 echo 
                 "</article>";
@@ -130,6 +130,96 @@
         echo
             "</div>
         </section>";
+
+        
+        // Mostramos las fotos favoritas
+        echo "
+        <section>
+        <h2>Foto Favorita</h2>
+        <div class='seccionfoto'>";
+
+        // Guardamos el contenido del fichero de fotos favoritas en un array
+        if (!($fichero = file("ficheros/fotosFav.txt"))  ||  count($fichero) == 0) {
+            echo "<h3>Todavía no hay ninguna foto favorita</h3>";
+        } else {
+
+            // Sacamos un numero aleatorio para determinar una foto favorita
+            $indexFotoFav = mt_rand(0, count($fichero)-1);
+            $datosFotoFav = explode("-", $fichero[$indexFotoFav]);
+            $idFoto = intval($datosFotoFav[0]);
+
+            // Obtenemos toda la información necesaria de la base de datos
+            $sentencia = "SELECT f.*, a.Titulo AS TAlbum, u.NomUsuario FROM fotos f, albumes a, usuarios u WHERE f.Album=a.IdAlbum AND u.IdUsuario=a.Usuario AND f.IdFoto=$idFoto";
+            if(!($resultado = $conexion->query($sentencia))) {
+                echo "<p>Error al ejecutar la sentencia <b>$sentencia</b>: " . $conexion->connect_error; 
+                echo '</p>'; 
+                exit;
+            }
+            $fila = $resultado->fetch_object();
+
+            // Mostramos la información en la página web
+            echo "<article>
+                    <a style='display:flex;' href='detalle.php?id=$fila->IdFoto'>
+                        <img style='margin:auto;' width='400' src='$fila->Fichero' alt='$fila->Alternativo'>
+                    </a>
+                    <h3 style='padding:10px;'><a href='detalle.php?id=$fila->IdFoto'>$fila->Titulo</a></h3>
+                    <p><b>Descripción</b>: $fila->Descripcion</p>";
+                    if ($fila->Fecha == "0000-00-00") {
+                        echo "<p><b>Fecha</b>: No consta</p>";
+                    } else {
+                        echo "<p><b>Fecha</b>: $fila->Fecha</p>";
+                    }
+                    echo "<p><b>Fecha de registro</b>: $fila->FRegistro</p>";
+
+                    //Si no tiene pais, no mostramos ese campo.
+                    $sentencia = "SELECT p.NomPais FROM fotos f, paises p WHERE f.Pais=p.IdPais AND f.IdFoto=$idFoto";
+                    if(!($resultado = $conexion->query($sentencia))) {
+                        echo "<p>Error al ejecutar la sentencia <b>$sentencia</b>: " . $conexion->connect_error; 
+                        echo '</p>'; 
+                        exit;
+                    }
+                    $pais = $resultado->fetch_object();
+                    if ($resultado->num_rows) {
+                        echo "<p><b>País</b>: $pais->NomPais</p>";
+                    } else {
+                        echo "<p><b>País</b>: No consta</p>";
+                    }
+            echo "<p><b>Álbum</b>: $fila->TAlbum</p>
+                <p><b>Usuario</b>: $fila->NomUsuario</p>
+                <p><b>Crítico de Fotografía</b>: $datosFotoFav[1]</p>
+                <p><b>Comentario</b>: $datosFotoFav[2]</p>";
+            echo 
+            "</article>";
+        }
+
+        echo "</div></section>";
+
+
+
+
+        // Mostramos los consejos fotográficos
+        echo "
+        <section>
+        <h2>Consejo Fotográfico</h2>";
+
+        // Obtenemos el contenido del archivo JSON
+        $contenidoConsejos = file_get_contents("ficheros/consejos.json");
+
+        // Convertimos el contenido JSON en un array PHP
+        $consejos = json_decode($contenidoConsejos, true);
+        $indexConsejo = mt_rand(0, count($consejos)-1);
+        $consejo = $consejos[$indexConsejo];
+        $categoria = $consejo["Categoria"];
+        $dificultad = $consejo["Dificultad"];
+        $comentario = $consejo["Consejo"];
+
+        // Lo mostramos en la página web
+        echo "<p><b>Categoría</b>: $categoria</p>";
+        echo "<p><b>Dificultad</b>: $dificultad</p>";
+        echo "<p><b>Consejo</b>: $comentario</p>";
+
+        echo "</section>";
+        
 
         $resultado->close();
         $conexion->close();
